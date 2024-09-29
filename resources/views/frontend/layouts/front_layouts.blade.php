04abc57d51167f8f88706844b8e358a1565b2c07
Mutasim556 <134157496+Mutasim556@users.noreply.github.com>
2024-09-29 17:03:17 +0600

    test

--- resources/views/frontend/layouts/front_layouts.blade.php
+++ resources/views/frontend/layouts/front_layouts.blade.php
@@ -462,115 +462,16 @@
     <!--End header-->
     @yield('body')
     <footer class="main">
-        <section class="newsletter mb-15 wow animate__animated animate__fadeIn">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="position-relative newsletter-inner">
-                            <div class="newsletter-content">
-                                <h2 class="mb-20">
-                                    Stay home & get your daily <br />
-                                    needs from our shop
-                                </h2>
-                                <p class="mb-45">Start You'r Daily Shopping with <span class="text-brand">Nest
-                                        Mart</span></p>
-                                <form class="form-subcriber d-flex">
-                                    <input type="email" placeholder="Your emaill address" />
-                                    <button class="btn" type="submit">Subscribe</button>
-                                </form>
-                            </div>
-                            <img src="assets/imgs/banner/banner-9.png" alt="newsletter" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="featured section-padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay="0">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-1.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Best prices & offers</h3>
-                                <p>Orders $50 or more</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay=".1s">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-2.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Free delivery</h3>
-                                <p>24/7 amazing services</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay=".2s">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-3.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Great daily deal</h3>
-                                <p>When you sign up</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay=".3s">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-4.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Wide assortment</h3>
-                                <p>Mega Discounts</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay=".4s">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-5.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Easy returns</h3>
-                                <p>Within 30 days</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
-                        <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp"
-                            data-wow-delay=".5s">
-                            <div class="banner-icon">
-                                <img src="assets/imgs/theme/icons/icon-6.svg" alt="" />
-                            </div>
-                            <div class="banner-text">
-                                <h3 class="icon-box-title">Safe delivery</h3>
-                                <p>Within 30 days</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        
+        
         <section class="section-padding footer-mid">
             <div class="container pt-15 pb-20">
-                <div class="row">
+                <div class="row" style="border-top:1px solid #198754">
                     <div class="col">
                         <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp"
                             data-wow-delay="0">
                             <div class="logo mb-30">
-                                <a href="index.html" class="mb-15"><img src="assets/imgs/theme/logo.svg"
+                                <a href="index.html" class="mb-15"><img src="{{ asset('public/assets/imgs/theme/logo.svg') }}"
                                         alt="logo" /></a>
                                 <p class="font-lg text-heading">Awesome grocery store website template</p>
                             </div>
@@ -590,58 +491,49 @@
                         </div>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp"
-                        data-wow-delay=".1s>
+                        data-wow-delay=".1s">
                         <h4 class=" widget-title">Company</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
                             <li><a href="#">About Us</a></li>
-                            <li><a href="#">Delivery Information</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms &amp; Conditions</a></li>
+                            <li><a href="#">Company Profile</a></li>
+                            <li><a href="#">Investor Relations</a></li>
+                            <li><a href="#">Cybersecurity</a></li>
+                            <li><a href="#">Compliance</a></li>
+                            <li><a href="#">Sustainability</a></li>
+                            <li><a href="#">Focused on Quality</a></li>
                             <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Support Center</a></li>
-                            <li><a href="#">Careers</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp"
                         data-wow-delay=".2s">
-                        <h4 class="widget-title">Account</h4>
+                        <h4 class="widget-title">Newsroom</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Sign In</a></li>
-                            <li><a href="#">View Cart</a></li>
-                            <li><a href="#">My Wishlist</a></li>
-                            <li><a href="#">Track My Order</a></li>
-                            <li><a href="#">Help Ticket</a></li>
-                            <li><a href="#">Shipping Details</a></li>
-                            <li><a href="#">Compare products</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Latest News</a></li>
+                            <li><a href="#">Success Stories</a></li>
+                            <li><a href="#">Video Library</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp"
                         data-wow-delay=".3s">
-                        <h4 class="widget-title">Corporate</h4>
+                        <h4 class="widget-title">Events</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Become a Vendor</a></li>
-                            <li><a href="#">Affiliate Program</a></li>
-                            <li><a href="#">Farm Business</a></li>
-                            <li><a href="#">Farm Careers</a></li>
-                            <li><a href="#">Our Suppliers</a></li>
-                            <li><a href="#">Accessibility</a></li>
-                            <li><a href="#">Promotions</a></li>
+                            <li><a href="#">Event List</a></li>
+                            <li><a href="#">Hikvision Live</a></li>
                         </ul>
                     </div>
                     <div class="footer-link-widget col wow animate__animated animate__fadeInUp"
                         data-wow-delay=".4s">
-                        <h4 class="widget-title">Popular</h4>
+                        <h4 class="widget-title">Quick Links</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
-                            <li><a href="#">Milk & Flavoured Milk</a></li>
-                            <li><a href="#">Butter and Margarine</a></li>
-                            <li><a href="#">Eggs Substitutes</a></li>
-                            <li><a href="#">Marmalades</a></li>
-                            <li><a href="#">Sour Cream and Dips</a></li>
-                            <li><a href="#">Tea & Kombucha</a></li>
-                            <li><a href="#">Cheese</a></li>
+                            <li><a href="#">Hikvision eLearning</a></li>
+                            <li><a href="#">Where to Buy</a></li>
+                            <li><a href="#">Discontinued Products</a></li>
+                            <li><a href="#">Core Technologies</a></li>
+                            <li><a href="#">Sitemap</a></li>
                         </ul>
                     </div>
-                    <div class="footer-link-widget widget-install-app col wow animate__animated animate__fadeInUp"
+                    {{-- <div class="footer-link-widget widget-install-app col wow animate__animated animate__fadeInUp"
                         data-wow-delay=".5s">
                         <h4 class="widget-title">Install App</h4>
                         <p class="">From App Store or Google Play</p>
@@ -653,7 +545,7 @@
                         </div>
                         <p class="mb-20">Secured Payment Gateways</p>
                         <img class="" src="assets/imgs/theme/payment-method.png" alt="" />
-                    </div>
+                    </div> --}}
                 </div>
         </section>
         <div class="container pb-30 wow animate__animated animate__fadeInUp" data-wow-delay="0">
@@ -662,8 +554,7 @@
                     <div class="footer-bottom"></div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6">
-                    <p class="font-sm mb-0">&copy; 2022, <strong class="text-brand">Nest</strong> - HTML Ecommerce
-                        Template <br />All rights reserved</p>
+                    <p class="font-sm mb-0">&copy; {{ date('Y') }}, <strong class="text-brand">{{ env('COMPANY_NAME') }}</strong> <br />All rights reserved</p>
                 </div>
                 <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                     <div class="hotline d-lg-inline-flex mr-30">
@@ -678,15 +569,15 @@
                 <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                     <div class="mobile-social-icon">
                         <h6>Follow Us</h6>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-facebook-white.svg"
+                        <a href="#"><img src="{{ asset('public/assets/imgs/theme/icons/icon-facebook-white.svg') }}"
                                 alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-twitter-white.svg"
+                        <a href="#"><img src="{{ asset('public/assets/imgs/theme/icons/icon-twitter-white.svg') }}"
                                 alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-instagram-white.svg"
+                        <a href="#"><img src="{{ asset('public/assets/imgs/theme/icons/icon-instagram-white.svg') }}"
                                 alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-pinterest-white.svg"
+                        <a href="#"><img src="{{ asset('public/assets/imgs/theme/icons/icon-pinterest-white.svg') }}"
                                 alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-youtube-white.svg"
+                        <a href="#"><img src="{{ asset('public/assets/imgs/theme/icons/icon-youtube-white.svg') }}"
                                 alt="" /></a>
                     </div>
                     <p class="font-sm">Up to 15% discount on your first subscribe</p>
@@ -834,26 +725,24 @@
 
         function visible_support(x){
             if(localStorage.getItem('support_item_'+x)){
-                    var scat='';
+                    var scat=`<li class="sub-mega-menu col-4 mb-4"><a class="menu-title" href="#"></a><ul>`;
                     $.each(JSON.parse(localStorage.getItem('support_item_'+x)),function(cat_key,cat_val){
-                        scat = scat+`<li><a href="#">${cat_val.name}</a></li>`;
+                        scat = scat+'<li><a href="#">'+cat_val.name+'</a></li>';
                     });
-
-                    $('#support_visibility li ul').empty().append(scat);
+                    scat = scat + '</ul></li>';
+                    $('#support_visibility').empty().append(scat);
             }else{
-               
-
                 $.ajax({
                 type: "get",
                 url: "{{ route('frontend.getSupportCategoryDetails') }}?id="+x,
                 success: function (data) {
                     localStorage.setItem('support_item_'+x,JSON.stringify(data.categories))
-                    var scat = '';
+                    var scat = `<li class="sub-mega-menu col-4 mb-4"><a class="menu-title" href="#"></a><ul>`;
                     $.each(data.categories,function(cat_key,cat_val){
                         scat = scat+`<li><a href="#">${cat_val.name}</a></li>`;
                     });
-
-                    $('#support_visibility li ul').empty().append(scat);
+                    scat = scat + '</ul></li>';
+                    $('#support_visibility').empty().append(scat);
                 },
                 error: function (err) {
                     var err_message = err.responseJSON.message.split("(");
